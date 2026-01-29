5564c1d1d8e60d85f29340c4bb4cc8845009873a
Niq-Debug <aniquwais959@gmail.com>
2026-01-29 23:49:07 +0800

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  document.getElementById("formMessage").textContent =
+    "Thank you for contacting ABC Secondary School.";
+});
